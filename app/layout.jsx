64eb50b14ae2f3b97d4020a489b957a53c23cc4b
RuzbihanZaleek <ruzbihan.z@eyepax.com>
2024-02-13 12:44:36 +0530

    2. wrapping layout from Provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,27 @@
-import '@styles/global.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
+};
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+      <Provider>
+        <body>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">{children}</main>
-      </body>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </body>
+      </Provider>
     </html>
   );
 };
